7f4e4d12b69320567b45b823d6dfd37400d3522c
alan94290 <alan94290@yahoo.com.tw>
2020-06-19 20:34:50 +0800

    First commit

---
+++ onlineshopdemo/src/main/java/com/alan/springboot/onlineshopdemo/OnlineshopdemoApplication.java
@@ -0,0 +1,13 @@
+package com.alan.springboot.onlineshopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopdemoApplication.class, args);
+	}
+
+}
---
+++ onlineshopdemo/src/test/java/com/alan/springboot/onlineshopdemo/OnlineshopdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.springboot.onlineshopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
