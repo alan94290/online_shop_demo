37a87dc9109f4010642b1dd159e439d200c10c2d
alan94290 <alan94290@yahoo.com.tw>
2020-06-20 08:58:06 +0800

    redirect to index page

--- onlineshopdemo/src/main/java/com/alan/springboot/onlineshopdemo/controller/IndexController.java
+++ onlineshopdemo/src/main/java/com/alan/springboot/onlineshopdemo/controller/IndexController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+	@GetMapping("/")
+	public String redirectIndex(){
+		return "redirect:index";
+	}
+
 	@GetMapping("/index")
 	public String showIndex(Model model){
 		model.addAttribute("datetime",new java.util.Date());
