61e588626886c29ba89379cd2c4fb27d779b3051
alan94290 <alan94290@yahoo.com.tw>
2020-06-20 15:49:31 +0800

    Reupload

--- onlineshopdemo/src/main/java/com/alan/springboot/onlineshopdemo/controller/IndexController.java
+++ onlineshopdemo/src/main/java/com/alan/springboot/onlineshopdemo/controller/IndexController.java
@@ -1,20 +1,13 @@
 package com.alan.springboot.onlineshopdemo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
 	@GetMapping("/")
-	public String redirectIndex(){
-		return "redirect:index";
-	}
-
-	@GetMapping("/index")
-	public String showIndex(Model model){
-		model.addAttribute("datetime",new java.util.Date());
+	public String showIndex(){
 		return "index";
 	}
 }
